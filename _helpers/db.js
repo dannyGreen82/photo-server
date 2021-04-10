d51a973a6ddb5dcf7a102c761b8b5adeaac66f0c
dgreen <daniel.green@sirsidynix.com>
2021-04-10 00:05:19 -0500

    Update project name

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,5 +1,6 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
+console.log('Database connection url: ' + config.connectionString);
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
 mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
 mongoose.Promise = global.Promise;
